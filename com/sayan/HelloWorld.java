f8072ec0fca8abd088d3ff593c658d7fd65db263
rims94 <saayaanghosh@gmail.com>
2020-04-30 11:06:48 +0530

    initializing git to local eclipse java folder

---
+++ com/sayan/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sayan;
+
+/**
+ * @author rims94
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World from ubuntu 20.04 lts!");
+	}
+
+}
